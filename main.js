bf693cc7e011f864e8973752d5bd22e84636e154
shaddog <shadowDogie@proton.me>
2023-06-28 02:45:58 +0400

    fixing edge cases

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 import { initWallet } from "./wallet.js"
 import { getSomeRealSh } from "./blockchain.js"
 import { fillInPageInfo } from "./view.js"
-import { verifyUser, verifyInviter } from "./verify.js";
+import { verifyUser, verifyInviter, verifyVerify } from "./verify.js";
 
 const tonConnectUI = initWallet()
 
 tonConnectUI.connectionRestored.then(async (restored) => {
-  if (!restored) {
+  if (restored) {
+    verifyVerify(tonConnectUI.wallet.account.address)
+  } else {
     verifyInviter()
   }
 })
 
 const unsubscribe = tonConnectUI.onStatusChange(
   async (walletInfo) => {
-    const userAddress = walletInfo.account.address
+    const userAddress = walletInfo?.account?.address
 
     await verifyUser(userAddress)
   }
--- verify.js
+++ verify.js
@@ -9,7 +9,7 @@ export const verifyUser = async (userAddress) => {
   if(userRefAddress && isUserRefActive) {
     window.location.hash = userFriendlyAddress
     showUserRef(userRefAddress)
-  }  
+  }
 }
 
 export const verifyInviter = async () => {
@@ -19,6 +19,21 @@ export const verifyInviter = async () => {
     const isInviterRefActive = await areYouIn(inviterRefAddress)
   
     if(inviterRefAddress && isInviterRefActive) {
+      showInviterRef(inviterAddress, inviterRefAddress)
+    }
+  }
+}
+
+export const verifyVerify = async (userAddress) => {
+  if(window.location.hash) {
+    const inviterAddress = window.location.hash.split('#')[1]
+    const inviterRefAddress = await getRefAddress(inviterAddress)
+    const isInviterRefActive = await areYouIn(inviterRefAddress)
+
+    const userRefAddress = await getRefAddress(userAddress)
+    const isUserRefActive = await areYouIn(userRefAddress)
+
+    if(userRefAddress && !isUserRefActive && isInviterRefActive) {
       showInviterRef(inviterAddress, inviterRefAddress)
     }
   }
